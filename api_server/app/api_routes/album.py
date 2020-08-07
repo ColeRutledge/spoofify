20c92674ebb3074021ffb75b82a0323ab545818b
marco <serranomarco@hotmail.com>
2020-08-07 14:10:42 -0700

    strict slashes

--- api_server/app/api_routes/album.py
+++ api_server/app/api_routes/album.py
@@ -6,23 +6,24 @@ bp = Blueprint('album', __name__, url_prefix="/api/album")
 
 
 # GET all albums on Spotify
-@bp.route("/", methods=["GET"])
+@bp.route("/", strict_slashes=False, methods=["GET"])
 def get_albums():
     albums = Album.query.all()
     # artist =[artist.to_dict() for artist in albums.artist]
     albums = [{"title": album.title, "id": album.id, "image_url": album.image_url,
-                "artist": album.artist.name} for album in albums]
-    
+               "artist": album.artist.name} for album in albums]
+
     return {"albums": albums}
 
 # GET one album
+
+
 @bp.route("/<int:id>", methods=["GET"])
 def get_album(id):
     album = Album.query.get(id)
     res = {"title": album.title,
            "artist": album.artist.name}
     return jsonify(res)
-
 
 
 # GET all songs for an album
@@ -34,16 +35,18 @@ def get_album_songs(id):
     album = Album.query.options(joinedload("songs")).get(id)
     songs = [song.to_dict() for song in album.songs]
     # payload = {"album": album}
-    payload = {"album": {"artist":album.artist.name, 
-                        "album_name": album.title,"songs": songs}}
+    payload = {"album": {"artist": album.artist.name,
+                         "album_name": album.title, "songs": songs}}
     return payload
 
 # GET one song from album
+
+
 @bp.route("/<int:id>/<int:song_id>")
-def get_album_song(id,song_id):
-    songs = Song.query.filter_by(album_id=id,id=song_id).all()
+def get_album_song(id, song_id):
+    songs = Song.query.filter_by(album_id=id, id=song_id).all()
     res = [{"title": song.title,
-    "album": song.album.title,
-    "song_length": song.song_length,
-    "song_url": song.song_url} for song in songs]
+            "album": song.album.title,
+            "song_length": song.song_length,
+            "song_url": song.song_url} for song in songs]
     return jsonify(res)
