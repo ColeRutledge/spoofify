176349271d6751ee7eb9213713b3949cfababbf4
ColeRutledge <ColeRutledge@gmail.com>
2020-08-09 10:41:17 -0400

    add gunicorn to entrypoint.sh

--- api_server/entrypoint.sh
+++ api_server/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 pip3 install -r /app/requirements.txt
 python3 entry.py db upgrade
-python3 entry.py run -h 0.0.0.0
-
-# gunicorn app:app
+gunicorn app:app
+# python3 entry.py run -h 0.0.0.0
