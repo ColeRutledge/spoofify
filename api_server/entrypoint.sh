86302076d3a2f67ec6d18ee85168736c3447a8cf
ColeRutledge <ColeRutledge@gmail.com>
2020-08-26 18:36:40 -0400

    config for development

--- api_server/entrypoint.sh
+++ api_server/entrypoint.sh
@@ -2,5 +2,5 @@
 pip3 install -r /app/requirements.txt
 python3 entry.py db upgrade
 python3 database.py
-gunicorn app:app
-# python3 entry.py run -h 0.0.0.0
+python3 entry.py run -h 0.0.0.0
+# gunicorn app:app
